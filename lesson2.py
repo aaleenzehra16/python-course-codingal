25b46e81e075e1677618d9cdb9923b655a1460db
aaleenzehra16 <aaleenzehra16@gmail.com>
2026-09-06 13:42:15 +0500

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,10 @@
+print(12)
+print("what is your name?")
+print("option1 \n option2")
+print("option1","option2",end="+")
+n=10
+a="age"
+age=input("what is your age?")
+print(f"my age is {n} \n","your input",a,age)
+import keyword
+print(keyword.kwlist)
